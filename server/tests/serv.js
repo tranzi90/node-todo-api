2555d0b61f53af10ec2aed374655e2ee3a8d0adc
tranzi90 <tranzi90@gmail.com>
2018-11-30 14:40:06 +0300

    Add GET /todos test

--- server/tests/serv.js
+++ server/tests/serv.js
@@ -4,8 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'test 1'
+}, {
+    text: 'test 2'
+}];
+
 beforeEach(function (done) {
-    Todo.deleteMany({}).then(() => done());
+    Todo.deleteMany({})
+        .then(function () {
+            Todo.insertMany(todos);
+        })
+        .then(() => done());
 })
 
 describe('POST /todos', function () {
@@ -23,7 +33,7 @@ describe('POST /todos', function () {
                 if (err)
                     return done(err);
 
-                Todo.find().then(function (todos) {
+                Todo.find({text}).then(function (todos) {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -41,9 +51,21 @@ describe('POST /todos', function () {
                     return done(err);
 
                 Todo.find().then(function (todos) {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
+    });
+});
+
+describe('GET /todos', function () {
+    it('should get all todos', function (done) {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect(function (res) {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
     });
 });
