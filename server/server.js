a661c485116f0226e9c8fb4b52d5ff106ef49b6e
tranzi90 <tranzi90@gmail.com>
2018-12-11 15:24:19 +0300

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-const {ObjectID} = require('mongodb');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -52,6 +53,28 @@ app.delete('/todos/:id', function (req, res) {
         return res.status(404).send();
 
     Todo.findByIdAndDelete(id).then(function (todo) {
+        if (!todo)
+            return res.status(404).send();
+
+        res.send({todo});
+    }).catch(() => res.status(400).send());
+});
+
+app.patch('/todos/:id', function (req, res) {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id))
+        return res.status(404).send();
+
+    if (_.isBoolean(body.completed) && body.completed)
+        body.completedAt = new Date().getTime();
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(function (todo) {
         if (!todo)
             return res.status(404).send();
 
